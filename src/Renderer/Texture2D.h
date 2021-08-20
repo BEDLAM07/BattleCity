a76a5e6bddfa7e5cadefa77b0d8d6cae606b6320
Сергей Ростокин <rostockin.serg@yandex.ru>
2021-08-20 22:25:06 +0300

    New code

--- src/Renderer/Texture2D.h
+++ src/Renderer/Texture2D.h
@@ -7,6 +7,7 @@ namespace Renderer {
 	class Texture2D
 	{
 	public:
+		// передаем ширину текстуры и ее высоту
 		Texture2D(const GLuint width, GLuint height, 
 						const unsigned char* data, 
 						const unsigned int channels = 4, 
@@ -20,9 +21,13 @@ namespace Renderer {
 		Texture2D(Texture2D&& texture2d);
 		~Texture2D();
 
+		//подключаем  текстуры
 		void bind() const;
-
 	private:
+		GLuint m_ID;
+		GLuint m_mode;
+		unsigned int m_width;
+		unsigned int m_height;
 
 	};
 }
